51e1ae228e03529bdb7974d3697e43c15a7b4f7a
emersonilan <emerson.ilan@aluno.senac.rio>
2026-04-28 19:20:25 -0300

    quarto commit

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
+from .models import Post
+
+
 def home(request):
-    return render(request, 'home.html')
+
+    posts = Post.objects.all()
+
+    return render(
+        request,
+        'home.html',
+        {
+            'posts': posts
+        }
+    )
+
 
 def sobre(request):
     return render(request, 'sobre.html')
